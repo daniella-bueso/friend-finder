c6be8475c5b8bcc4dd8c19cb9428babea9e9dcd8
Daniella D <danielladevoto@gmail.com>
2018-02-07 20:27:00 -0500

    working on html pages

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,3 +3,17 @@ var friends = [
     photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores:[5,1,4,4,5,1,2,5,4,1]
     }];
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+// Here we export the array. This makes it accessible to other files using require. 
+    module.exports = friends;
